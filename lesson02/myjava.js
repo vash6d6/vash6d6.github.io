1c3a119f68122bb745c0780ae17857ee751ea34d
Vash6D6 <44424278+vash6d6@users.noreply.github.com>
2018-10-24 21:01:54 -0700

    age difference

--- lesson02/myjava.js
+++ lesson02/myjava.js
@@ -11,18 +11,3 @@ document.getElementById("lastMod").innerHTML = document.lastModified;
 function myAlert() {
     alert("Thanks for joining the newsletter! We will be in contact shortly.");
 }
-
-//Age verification prompt
-document.getElementById("age").value;
-
-function agePrompt() 
-{
-    var agePrompt=prompt("What is your age?");
-    
-    if (agePrompt>=18) alert('Welcome!');
-    else 
-    {
-        alert('Sorry! Come back in a few years!');
-        document.location="http://www.codelibary.com/";
-    }
-}
